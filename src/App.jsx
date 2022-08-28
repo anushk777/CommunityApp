0f006888b7698a036abcf98b55863fab14a2f898
LahsivK4070 <vishalkumarcoold2@gmail.com>
2022-08-28 12:16:57 +0530

    vishal- login and signup form updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
 import Login from "./components/login/Login.js";
-import Signup from "./components/signup/Signup.js";
 function App() {
   return (
-    <Signup />
+    <Login />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import App from './App';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
 );
